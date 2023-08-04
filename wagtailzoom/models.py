b5fc232c2c68f08e56a6557ba0405c318be1d5c4
Erick Otenyo <otenyo.erick@gmail.com>
2023-08-04 17:41:27 +0300

    Refactor for inheritance resolutions

--- wagtailzoom/models.py
+++ wagtailzoom/models.py
@@ -3,10 +3,7 @@ import json
 from django.core.mail import mail_admins
 from django.db import models
 from django.template import Context, Template
-from django.template.response import TemplateResponse
-from django.utils.decorators import method_decorator
 from django.utils.translation import gettext as _
-from django.views.decorators.csrf import csrf_exempt
 from wagtail.admin.panels import FieldPanel
 from wagtail.contrib.forms.models import AbstractForm
 from wagtail.contrib.settings.models import BaseSiteSetting
@@ -54,11 +51,11 @@ class AbstractZoomIntegrationForm(AbstractForm):
 
     @property
     def zoom_event_id(self):
-        return self.get_data().get("event_id")
+        return self.get_zoom_data().get("event_id")
 
     @property
     def zoom_event_type(self):
-        return self.get_data().get("event_type")
+        return self.get_zoom_data().get("event_type")
 
     @property
     def zoom_merge_fields(self):
@@ -69,7 +66,7 @@ class AbstractZoomIntegrationForm(AbstractForm):
                 pass
         return {}
 
-    def get_data(self):
+    def get_zoom_data(self):
         data = {}
         if self.zoom_event:
             try:
@@ -86,40 +83,19 @@ class AbstractZoomIntegrationForm(AbstractForm):
 
         return data
 
-    def post_process_submission(self, form, form_submission):
-        pass
-
-    def process_form_submission(self, form, request=None):
+    def process_form_submission(self, form):
         form_submission = super(AbstractZoomIntegrationForm, self).process_form_submission(form)
-        try:
-            self.post_process_submission(form, form_submission)
-        except Exception as e:
-            pass
+
+        if self.request:
+            self.zoom_integration_operation(self, form=form, request=self.request)
 
         return form_submission
 
-    def should_process_form(self, request, form_data):
-        return True
-
-    @method_decorator(csrf_exempt)
     def serve(self, request, *args, **kwargs):
-        if request.method == 'POST':
-            form = self.get_form(request.POST, request.FILES, page=self, user=request.user)
+        # We need to access the request later on in integration operation
+        self.request = request
 
-            if form.is_valid():
-                form_submission = None
-                if self.should_process_form(request, form.cleaned_data):
-                    form_submission = self.process_form_submission(form)
-                    self.integration_operation(self, form=form, request=request)
-
-                # render landing page
-                return self.render_landing_page(request, form_submission, *args, **kwargs)
-
-        form = self.get_form(page=self, user=request.user)
-        context = self.get_context(request)
-        context['form'] = form
-
-        return TemplateResponse(request, self.get_template(request), context)
+        return super(AbstractZoomIntegrationForm, self).serve(request, *args, **kwargs)
 
     def format_zoom_form_submission(self, form):
         formatted_form_data = {}
@@ -127,7 +103,7 @@ class AbstractZoomIntegrationForm(AbstractForm):
             formatted_form_data[k.replace('-', '_')] = v
         return formatted_form_data
 
-    def integration_operation(self, instance, **kwargs):
+    def zoom_integration_operation(self, instance, **kwargs):
         success = False
         response = None
         request = kwargs.get('request', None)
@@ -153,7 +129,7 @@ class AbstractZoomIntegrationForm(AbstractForm):
                 # mark as failed
                 success = False
 
-                data = json.dumps(self.get_data())
+                data = json.dumps(self.get_zoom_data())
 
                 message = "Error \n {}\n  Rendered \n {}\n Zoom Form Data\n {}".format(str(e),
                                                                                        str(rendered_dictionary),
@@ -162,7 +138,7 @@ class AbstractZoomIntegrationForm(AbstractForm):
 
         return success, response
 
-    def get_merge_fields_template(self):
+    def get_zoom_fields_template(self):
         fields = self.zoom_merge_fields
 
         for key, value in fields.items():
@@ -172,10 +148,10 @@ class AbstractZoomIntegrationForm(AbstractForm):
 
     def render_zoom_dictionary(self, form_submission):
 
-        merge_fields_templates = self.get_merge_fields_template()
+        fields_templates = self.get_zoom_fields_template()
 
         rendered_dictionary_template = json.dumps({
-            **merge_fields_templates,
+            **fields_templates,
         })
 
         rendered_dictionary = Template(rendered_dictionary_template).render(Context(form_submission))
